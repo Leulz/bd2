c65a69badc59b3019afc3b1b7e36da2a6c2dfc95
Léo Meira Vital <sir.meira@gmail.com>
2018-11-09 10:48:54 -0300

    Correção da letra a

--- 1/a.sql
+++ 1/a.sql
@@ -1,3 +1,3 @@
 SELECT nome, count(id_professor) AS QTD_PROF FROM (
-SELECT DISTINCT d.nome, aloc.id_professor FROM fat_alocacao aloc, dim_disciplina d WHERE aloc.id_disciplina = d.id_disciplina
-) GROUP BY nome ORDER BY QTD_PROF DESC, nome;
+SELECT DISTINCT d.id_disciplina, d.nome, aloc.id_professor FROM fat_alocacao aloc, dim_disciplina d WHERE aloc.id_disciplina = d.id_disciplina
+) GROUP BY id_disciplina, nome ORDER BY QTD_PROF DESC, nome;
