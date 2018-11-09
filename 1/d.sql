f3524b0fde2118c81b9da24b4f6738228801cad8
Léo Meira Vital <sir.meira@gmail.com>
2018-11-09 16:54:57 -0300

    Melhorando letra d com uso do INTERSECT

--- 1/d.sql
+++ 1/d.sql
@@ -1,5 +1,10 @@
-SELECT nome 
-FROM dim_professor p 
-WHERE EXISTS (SELECT 1 FROM fat_alocacao aloc WHERE p.id_professor = aloc.id_professor AND aloc.id_curso = 1)
-AND EXiSTS(SELECT 1 FROM fat_alocacao aloc WHERE p.id_professor = aloc.id_professor AND aloc.id_curso = 2)
+SELECT nome
+FROM dim_professor p, (SELECT id_professor 
+FROM fat_alocacao aloc 
+WHERE aloc.id_curso = 1
+INTERSECT
+SELECT id_professor
+FROM fat_alocacao aloc
+WHERE aloc.id_curso = 2) d
+WHERE p.id_professor = d.id_professor
 ORDER BY nome;
