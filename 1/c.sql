58ebe0a048cd851fb5d14585ace9cd5a8e2b1c86
Léo Meira Vital <sir.meira@gmail.com>
2018-11-16 16:12:34 -0300

    Delete c.sql

--- 1/c.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM 
- (SELECT p.nome, AVG(carga) as MEDIA_CH 
-  FROM dim_disciplina d, fat_alocacao a, dim_professor p 
-  WHERE a.id_disciplina = d.id_disciplina AND p.id_professor = a.id_professor 
-  GROUP BY p.nome 
-  ORDER BY MEDIA_CH DESC) 
-WHERE ROWNUM <= 5;
