944bd6b7b3538e24a53b32da8f6be9c6c43b4b69
Léo Meira Vital <sir.meira@gmail.com>
2018-11-16 16:12:28 -0300

    Delete b.sql

--- 1/b.sql
+++
@@ -1,3 +0,0 @@
-SELECT nome FROM dim_professor p1 
-WHERE p1.nome != ALL(SELECT DISTINCT p2.nome FROM fat_alocacao aloc, dim_professor p2 WHERE aloc.id_curso = 2 AND aloc.id_professor = p2.id_professor) 
-ORDER BY p1.nome;
