c96452a006022b828f71fa0eb84e803d1f0ef273
Léo Meira Vital <sir.meira@gmail.com>
2018-11-16 16:12:48 -0300

    Delete e.sql

--- 1/e.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM (SELECT s.codigo, COUNT(1) as COUNT_SALA 
-FROM dim_sala s, fat_alocacao aloc 
-WHERE aloc.id_sala = s.id_sala 
-GROUP BY s.codigo 
-ORDER BY COUNT_SALA DESC)
-WHERE ROWNUM <= 5;
